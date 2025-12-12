8b484c5f30cbeb1db4cda5094bcd4a445117628f
Sky201135 <kazisakiya11@outlook.com>
2025-12-12 22:20:02 +0600

    smth

---
+++ activity2.py
@@ -0,0 +1,6 @@
+def weather_condition():
+    print('The weather condition is pleasnt in:', spring)
+    print('The weather condition is same in:', autumn)
+spring = 'autumn'
+autumn = spring
+weather_condition()
